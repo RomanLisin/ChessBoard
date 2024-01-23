8f8cc123a3736ed3ae7579e31ba76f50f353e327
Lisin_sw23@top-academy.ru <Lisin_sw23@top-academy.ru>
2024-01-23 23:19:15 +1000

    Add project files.

---
+++ ChessBoard/main.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+using std::cout;
+using std::cin;
+using std::endl;
+
+void main() {
+	int n;
+	int r = 1; 
+
+	cout << "Enter board size: "; cin >> n;
+	cout << endl;
+
+	for (int i = 1; i <= n * n; i++) // go to the down
+	{
+		for (int j = 1; j <= n * n; j++) // go to the right
+		{
+			cout << (r % 2 == j % 2 ? "* " : "  ");
+			if (!(j % n)) r++; // if the square ends on the right
+			r++;
+		}
+		if (!(i % n)) r++; // if the square ends on the down
+		cout << endl;
+	}
+}
